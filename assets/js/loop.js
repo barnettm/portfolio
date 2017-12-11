000a5e35e584d5f5472701b460c94bd8bd85c8c7
barnettm <barnett717@gmail.com>
2017-12-11 14:03:34 -0800

    corrected sizing issue on aws icon

--- assets/js/loop.js
+++ assets/js/loop.js
@@ -315,121 +315,10 @@ jQuery(function ($) {
 
 	particlesJS('particles-js',
 	
-	// {
-	// 	"particles": {
-	// 	  "number": {
-	// 		"value": 80,
-	// 		"density": {
-	// 		  "enable": true,
-	// 		  "value_area": 800
-	// 		}
-	// 	  },
-	// 	  "color": {
-	// 		"value": "#ffffff"
-	// 	  },
-	// 	  "shape": {
-	// 		"type": "circle",
-	// 		"stroke": {
-	// 		  "width": 0,
-	// 		  "color": "#000000"
-	// 		},
-	// 		"polygon": {
-	// 		  "nb_sides": 5
-	// 		},
-	// 		"image": {
-	// 		  "src": "img/github.svg",
-	// 		  "width": 100,
-	// 		  "height": 100
-	// 		}
-	// 	  },
-	// 	  "opacity": {
-	// 		"value": 0.5,
-	// 		"random": false,
-	// 		"anim": {
-	// 		  "enable": false,
-	// 		  "speed": 1,
-	// 		  "opacity_min": 0.1,
-	// 		  "sync": false
-	// 		}
-	// 	  },
-	// 	  "size": {
-	// 		"value": 3,
-	// 		"random": true,
-	// 		"anim": {
-	// 		  "enable": false,
-	// 		  "speed": 40,
-	// 		  "size_min": 0.1,
-	// 		  "sync": false
-	// 		}
-	// 	  },
-	// 	  "line_linked": {
-	// 		"enable": true,
-	// 		"distance": 150,
-	// 		"color": "#ffffff",
-	// 		"opacity": 0.4,
-	// 		"width": 1
-	// 	  },
-	// 	  "move": {
-	// 		"enable": true,
-	// 		"speed": 2,
-	// 		"direction": "none",
-	// 		"random": false,
-	// 		"straight": false,
-	// 		"out_mode": "out",
-	// 		"bounce": false,
-	// 		"attract": {
-	// 		  "enable": false,
-	// 		  "rotateX": 600,
-	// 		  "rotateY": 1200
-	// 		}
-	// 	  }
-	// 	},
-	// 	"interactivity": {
-	// 	  "detect_on": "canvas",
-	// 	  "events": {
-	// 		"onhover": {
-	// 		  "enable": true,
-	// 		  "mode": "grab"
-	// 		},
-	// 		"onclick": {
-	// 		  "enable": true,
-	// 		  "mode": "repulse"
-	// 		},
-	// 		"resize": true
-	// 	  },
-	// 	  "modes": {
-	// 		"grab": {
-	// 		  "distance": 119.88011988011988,
-	// 		  "line_linked": {
-	// 			"opacity": 1
-	// 		  }
-	// 		},
-	// 		"bubble": {
-	// 		  "distance": 400,
-	// 		  "size": 40,
-	// 		  "duration": 2,
-	// 		  "opacity": 8,
-	// 		  "speed": 3
-	// 		},
-	// 		"repulse": {
-	// 		  "distance": 200,
-	// 		  "duration": 0.4
-	// 		},
-	// 		"push": {
-	// 		  "particles_nb": 4
-	// 		},
-	// 		"remove": {
-	// 		  "particles_nb": 2
-	// 		}
-	// 	  }
-	// 	},
-	// 	"retina_detect": true
-	//   })
-
 	{
 		"particles": {
 		  "number": {
-			"value": 468,
+			"value": 80,
 			"density": {
 			  "enable": true,
 			  "value_area": 800
@@ -454,27 +343,27 @@ jQuery(function ($) {
 			}
 		  },
 		  "opacity": {
-			"value": 1,
-			"random": true,
+			"value": 0.5,
+			"random": false,
 			"anim": {
-			  "enable": true,
+			  "enable": false,
 			  "speed": 1,
-			  "opacity_min": 0,
+			  "opacity_min": 0.1,
 			  "sync": false
 			}
 		  },
 		  "size": {
-			"value": 2,
+			"value": 3,
 			"random": true,
 			"anim": {
 			  "enable": false,
-			  "speed": 4,
-			  "size_min": 0.3,
+			  "speed": 40,
+			  "size_min": 0.1,
 			  "sync": false
 			}
 		  },
 		  "line_linked": {
-			"enable": false,
+			"enable": true,
 			"distance": 150,
 			"color": "#ffffff",
 			"opacity": 0.4,
@@ -482,16 +371,16 @@ jQuery(function ($) {
 		  },
 		  "move": {
 			"enable": true,
-			"speed": .5,
+			"speed": 2,
 			"direction": "none",
-			"random": true,
+			"random": false,
 			"straight": false,
 			"out_mode": "out",
 			"bounce": false,
 			"attract": {
 			  "enable": false,
 			  "rotateX": 600,
-			  "rotateY": 600
+			  "rotateY": 1200
 			}
 		  }
 		},
@@ -500,7 +389,7 @@ jQuery(function ($) {
 		  "events": {
 			"onhover": {
 			  "enable": true,
-			  "mode": "repulse"
+			  "mode": "grab"
 			},
 			"onclick": {
 			  "enable": true,
@@ -510,20 +399,20 @@ jQuery(function ($) {
 		  },
 		  "modes": {
 			"grab": {
-			  "distance": 400,
+			  "distance": 119.88011988011988,
 			  "line_linked": {
 				"opacity": 1
 			  }
 			},
 			"bubble": {
-			  "distance": 250,
-			  "size": 0,
+			  "distance": 400,
+			  "size": 40,
 			  "duration": 2,
-			  "opacity": 0,
+			  "opacity": 8,
 			  "speed": 3
 			},
 			"repulse": {
-			  "distance": 89.32849335314796,
+			  "distance": 200,
 			  "duration": 0.4
 			},
 			"push": {
@@ -536,6 +425,117 @@ jQuery(function ($) {
 		},
 		"retina_detect": true
 	  })
+
+	// {
+	// 	"particles": {
+	// 	  "number": {
+	// 		"value": 468,
+	// 		"density": {
+	// 		  "enable": true,
+	// 		  "value_area": 800
+	// 		}
+	// 	  },
+	// 	  "color": {
+	// 		"value": "#ffffff"
+	// 	  },
+	// 	  "shape": {
+	// 		"type": "circle",
+	// 		"stroke": {
+	// 		  "width": 0,
+	// 		  "color": "#000000"
+	// 		},
+	// 		"polygon": {
+	// 		  "nb_sides": 5
+	// 		},
+	// 		"image": {
+	// 		  "src": "img/github.svg",
+	// 		  "width": 100,
+	// 		  "height": 100
+	// 		}
+	// 	  },
+	// 	  "opacity": {
+	// 		"value": 1,
+	// 		"random": true,
+	// 		"anim": {
+	// 		  "enable": true,
+	// 		  "speed": 1,
+	// 		  "opacity_min": 0,
+	// 		  "sync": false
+	// 		}
+	// 	  },
+	// 	  "size": {
+	// 		"value": 2,
+	// 		"random": true,
+	// 		"anim": {
+	// 		  "enable": false,
+	// 		  "speed": 4,
+	// 		  "size_min": 0.3,
+	// 		  "sync": false
+	// 		}
+	// 	  },
+	// 	  "line_linked": {
+	// 		"enable": false,
+	// 		"distance": 150,
+	// 		"color": "#ffffff",
+	// 		"opacity": 0.4,
+	// 		"width": 1
+	// 	  },
+	// 	  "move": {
+	// 		"enable": true,
+	// 		"speed": .5,
+	// 		"direction": "none",
+	// 		"random": true,
+	// 		"straight": false,
+	// 		"out_mode": "out",
+	// 		"bounce": false,
+	// 		"attract": {
+	// 		  "enable": false,
+	// 		  "rotateX": 600,
+	// 		  "rotateY": 600
+	// 		}
+	// 	  }
+	// 	},
+	// 	"interactivity": {
+	// 	  "detect_on": "canvas",
+	// 	  "events": {
+	// 		"onhover": {
+	// 		  "enable": true,
+	// 		  "mode": "repulse"
+	// 		},
+	// 		"onclick": {
+	// 		  "enable": true,
+	// 		  "mode": "repulse"
+	// 		},
+	// 		"resize": true
+	// 	  },
+	// 	  "modes": {
+	// 		"grab": {
+	// 		  "distance": 400,
+	// 		  "line_linked": {
+	// 			"opacity": 1
+	// 		  }
+	// 		},
+	// 		"bubble": {
+	// 		  "distance": 250,
+	// 		  "size": 0,
+	// 		  "duration": 2,
+	// 		  "opacity": 0,
+	// 		  "speed": 3
+	// 		},
+	// 		"repulse": {
+	// 		  "distance": 89.32849335314796,
+	// 		  "duration": 0.4
+	// 		},
+	// 		"push": {
+	// 		  "particles_nb": 4
+	// 		},
+	// 		"remove": {
+	// 		  "particles_nb": 2
+	// 		}
+	// 	  }
+	// 	},
+	// 	"retina_detect": true
+	//   })
 
 
 	  createContactInfo();
